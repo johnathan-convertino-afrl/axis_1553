4d77c988130e7b41dafd7d92a704fcb1e5295b4c
Jay Convertino <johnathan.convertino.1@us.af.mil>
2025-06-26 17:14:02 -0400

    update manual

--- docs/manual/html/modules/SystemVerilog/tb_cocotb-Summary.js
+++ docs/manual/html/modules/SystemVerilog/tb_cocotb-Summary.js
@@ -1,1 +1,1 @@
-﻿NDFramePage.OnPageTitleLoaded("SystemVerilogModule:tb_cocotb","tb_cocotb");NDSummary.OnSummaryLoaded("SystemVerilogModule:tb_cocotb",[["SystemVerilog","SystemVerilog"]],[["Modules","Module"]],[[67,0,0,"tb_cocotb"]]);
+﻿NDFramePage.OnPageTitleLoaded("SystemVerilogModule:tb_cocotb","tb_cocotb");NDSummary.OnSummaryLoaded("SystemVerilogModule:tb_cocotb",[["SystemVerilog","SystemVerilog"]],[["Modules","Module"]],[[38,0,0,"tb_cocotb"]]);
